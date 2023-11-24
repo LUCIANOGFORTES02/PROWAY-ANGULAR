343f35197e417fb1b42c1dc3a7e525200aed960f
LUCIANOGFORTES02 <lucianogfortes@gmail.com>
2023-11-24 17:03:14 -0300

    angular

---
+++ PROWAY-COMPUTERS/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes } from '@angular/router';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+
+const routes : Routes = [
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+  {path:"",redirectTo:"produtos", pathMatch:"full"},
+  {path: "**", component: NaoEncontradoComponent }
+
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+    
+  ],
+  exports:[
+    RouterModule
+  ]
+
+})
+export class AppRoutingModule { }
---
+++ PROWAY-COMPUTERS/src/app/produtos/produtos.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IProduto, produtos } from '../product';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent {
+
+  produtos: IProduto[] = produtos;
+
+
+}
